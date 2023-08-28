34dc0270dc164b421c60640a347c20cc0221e525
Benazir Sultana <benazirsultana1998@gmail.com>
2023-08-28 23:29:45 +0530

    ref: env port added

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 import { connectdatabase } from './database/index.js';
 import Router from "./Routes/index.js"
-import express from "express";
+import express from "express"
+import { connectdatabase } from "./database/index.js"
+import Router from "./routes/auth.js"
 import cors from "cors"
+
 let app = express()
+app.use(cors()) 
 app.use(express.json())
-app.use(cors())
 app.get("/",(req,res)=>{
 res.send("server started")
 })
 
 
 app.use(Router)
-const port = 8080
+const port = process.env.PORT ||8080
 
 app.listen(port,async(req,res)=>{
-    await  connectdatabase()
+    
+  await  connectdatabase()
+     
         console.log(` listening on http://localhost:${port}`)
    
 })
